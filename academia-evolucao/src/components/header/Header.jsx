05c80f0ceea5841f4e7ca40bc385dcc54da15f7d
Samuel Ferla Iamarino <125980333+SamIamarino@users.noreply.github.com>
2025-03-08 14:14:34 -0300

    feat: implementação do header e seus estilos

--- academia-evolucao/src/components/header/Header.jsx
+++ academia-evolucao/src/components/header/Header.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import './headerStyles.css';
+import logo from '../../assets/logos/logo-azul-pretoDesktop.png'
 
 export default function Header(){
     return(
-        <h1></h1>
+    <header>
+        <img src={logo} alt="" className='logo'/>    
+        <div>
+            <ul>
+                <li><a href="#">Nosso Espaço</a></li>
+                <li><a href="#">Sobre nós</a></li>
+                <li><a href="#">Planos</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>    
+        </div>
+    </header>
     );
 }
