2c8ded2b3240b7e9485540a1cf91dcceee6f8260
Samuel Ferla Iamarino <125980333+SamIamarino@users.noreply.github.com>
2025-03-10 10:57:13 -0300

    feat: criação do component PlanoCard

--- academia-evolucao/src/App.jsx
+++ academia-evolucao/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/header/Header';
 import AboutCard from './components/aboutCard/AboutCard';
-
+import PlanoCard from './components/planoCard/PlanoCard';
 
 const App = () => {
   
@@ -9,6 +9,7 @@ const App = () => {
     <>
       <Header />
       <AboutCard />
+      <PlanoCard />
     </>
   );
 };
--- academia-evolucao/src/components/planoCard/PlanoCard.jsx
+++ academia-evolucao/src/components/planoCard/PlanoCard.jsx
@@ -1,8 +1,21 @@
-import React
- from "react";
-export default function PlanoCard (){
-    
+import './PlanoCard.css'
+import fotoExercicio from '../../assets/carafazendoexercicio.png'
+export default function PlanoCard ({plano,description1,description2}){
+
     return(
-        <h1>teste </h1>
+        <div className="plano-card">
+            <div>
+                <img src={fotoExercicio} alt="Imagem plano" className='plano-img'/>
+                <div className="plano-card-header">
+                    <h1>{plano} Plano semestral</h1>
+                </div>
+
+            </div>
+            <div className='plano-card-body'>
+                <p>{description1}testeasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</p>
+                <hr />
+                <p>{description2}testeasdadasdasdsadddddddddddddddddddddddddddddddddd</p>
+            </div>
+        </div>
     );
 }
