6524bb463e42ab9b8100ea4546a9ed68d1ef6179
Samuel Ferla Iamarino <125980333+SamIamarino@users.noreply.github.com>
2025-03-10 10:57:36 -0300

    style: alguns ajustes nos styles

--- academia-evolucao/src/components/aboutCard/AboutCard.jsx
+++ academia-evolucao/src/components/aboutCard/AboutCard.jsx
@@ -2,13 +2,13 @@ import './AboutCard.css';
 import peso from '../../assets/fotosCarrousel/peso.png'
 export default function AboutCard({photoUrl, cardTitle, cardText, cardAlt}){
     return(
-        <div className='card'>
-            <div className='card-header'>
+        <div className='about-card'>
+            <div className='about-card-header'>
                 <h1 className='title'>{cardTitle} Equipamentos de qualidade</h1>
                 <img src={peso} alt={cardAlt} className='icon'/>
             </div>
 
-            <div className='card-body'>
+            <div className='about-card-body'>
                 <h3>{cardText}  testecaasdsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaard</h3>
             </div>
         </div>
