e6082b99f288767d65b3cd0ba861cf0af055123f
Samuel Ferla Iamarino <125980333+SamIamarino@users.noreply.github.com>
2025-03-12 11:14:41 -0300

    style: footer styles updated

--- academia-evolucao/src/components/footer/Footer.jsx
+++ academia-evolucao/src/components/footer/Footer.jsx
@@ -6,18 +6,24 @@ import faceLogo from '../../assets/imgensMidia/face.png'
 
 export default function Footer(){
     return(
-        <div className='footer'>
+        <footer className='footer'>
             <div className='footer-card'>
                 <img src={logoFooter} alt="Logo evolução" className='logo'/>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem labore</p>
-                <div>
-                    <img src={faceLogo} alt="face-icon" className='midia-icon'/>
+
+                <div className='text-container'> 
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem labore</p>
+                </div>
+
+                <div className='icon-container'>
+                    {/* mudar pra svg para aplicar os estilos certos e isso tem que ser 'wrapped com um tag <a></a> */}
+                    <img src={faceLogo} alt="face-icon" className='midia-icon'/> 
                     <img src={instaLogo} alt="insta-icon" className='midia-icon'/>
                     <img src={whatsLogo} alt="whats-icon" className='midia-icon'/>
                 </div>
             </div>
+            <hr />
 
-            <p>&copy; 2025 [SamIamarino]. Todos os direitos reservados.</p>
-        </div>
+            {/* <p>&copy; 2025 [SamIamarino]. Todos os direitos reservados.</p> */}
+        </footer>
     )
 }
