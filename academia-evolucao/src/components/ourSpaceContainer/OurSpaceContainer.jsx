4f5e0f5d3f79ff752c0cc439b1f2c9ed9233cff9
Samuel Ferla Iamarino <125980333+SamIamarino@users.noreply.github.com>
2025-03-21 10:27:09 -0300

    feat: adição do OurSpace Component e layout Responsivo

---
+++ academia-evolucao/src/components/ourSpaceContainer/OurSpaceContainer.jsx
@@ -0,0 +1,14 @@
+import PhotoCarrousel from "../photoCarrousel/PhotoCarrousel";
+import { CARROUSEL_IMAGES } from "../../data";
+import './OurSpaceContainer.css';
+// Provavelmente vai ser melhor mudar o nome desse component mais pra frente. 
+//Vai ter uma variavel para receber as infos do data.js aqui.
+
+export default function OurSpaceContainer({title}){
+    return(
+        <>
+            <h1 className="our-space-title" style={{marginBottom: "40px"}}>{title}</h1>
+            <PhotoCarrousel photoArray={CARROUSEL_IMAGES}/>
+        </>
+    );
+}
