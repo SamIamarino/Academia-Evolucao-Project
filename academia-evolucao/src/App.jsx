9eb7483e286f4d38acf03603e6d5e9fcb86f93cc
Samuel Ferla Iamarino <125980333+SamIamarino@users.noreply.github.com>
2025-04-01 15:05:10 -0300

    fix: bug de nao responder a mudança de tema resolvido

--- academia-evolucao/src/App.jsx
+++ academia-evolucao/src/App.jsx
@@ -18,12 +18,29 @@ const App = () => {
   // precisa fazer o background mudar caso esteja branco dnv
   // só procurar em outro useEffect q ta certo.
   useEffect(() => {
-    if(isDarkTheme){
-      document.body.style.backgroundColor = "#1C1C1C";
-    }else{
-      document.body.style.backgroundColor = "#white";
-    }
-  }, [isDarkTheme])
+    const updateValues = () => {
+
+        const darkTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;    
+        setIsDarkTheme(darkTheme);
+
+
+        if(isDarkTheme || darkTheme){
+            document.body.style.backgroundColor = "#1C1C1C";
+        }else{
+            document.body.style.backgroundColor = "#white";
+        }
+
+    };
+
+    updateValues();
+
+    const themeWatcher = window.matchMedia("(prefers-color-scheme: dark)");
+    themeWatcher.addEventListener("change", updateValues);
+
+    return () => {
+      themeWatcher.removeEventListener("change", updateValues);
+    };
+  }, [])
   
   return (
     <>
