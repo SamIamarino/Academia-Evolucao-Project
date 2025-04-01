141eddc507f22f0175496d396d9ecfe3c95bed30
Samuel Ferla Iamarino <125980333+SamIamarino@users.noreply.github.com>
2025-04-01 15:06:13 -0300

    fix: About container , botao arrumado e colocado para mobile tb

--- academia-evolucao/src/components/aboutContainer/AboutContainer.jsx
+++ academia-evolucao/src/components/aboutContainer/AboutContainer.jsx
@@ -2,21 +2,33 @@ import './AboutContainer.css';
 import AboutCard from '../aboutCard/AboutCard';
 import { Carousel } from 'antd';
 import { useState, useEffect } from 'react';
-
+import { ArrowRightOutlined, DoubleRightOutlined } from '@ant-design/icons'
 const desktopContent = (
-    <div className='about-card-container'>
-        <AboutCard />
-        <AboutCard />
-        <AboutCard />
+    <div className='main-about-container'>
+        <div className='about-container'>
+            <AboutCard />
+            <AboutCard />
+            <AboutCard />
+            
+        </div>
+        <div className='button-container'>
+            <a href="/aboutUs" className='more-button'>Saiba Mais <DoubleRightOutlined /></a>
+        </div>
     </div>
 );
 
 const mobileContent = (
-    <Carousel draggable dots={false} arrows>
-        <AboutCard />   
-        <AboutCard />   
-        <AboutCard />   
-    </Carousel>
+    <>
+        <Carousel draggable dots={false} arrows>
+            <AboutCard />   
+            <AboutCard />   
+            <AboutCard />   
+        </Carousel>
+
+        <div className='button-container'>
+            <a href="/aboutUs" className='more-button'>Saiba Mais <DoubleRightOutlined /></a>
+        </div>
+    </>
 );
 
 export default function AboutContainer() {
@@ -28,8 +40,15 @@ export default function AboutContainer() {
         };
 
         window.addEventListener("resize", updateValues);
-        return () => window.removeEventListener("resize", updateValues);
-    }, []);
+        updateValues(); // Ensure it updates immediately when the effect runs
+
+        return () => {
+            window.removeEventListener("resize", updateValues);
+        };
+    }, []); // Run only once on mount
+
+    console.log(isMobile);
+
 
     return (
         <div className='about-container'>
