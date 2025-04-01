6a941ac98ed103d8cdbfe73a9faa6067aea436f2
Samuel Ferla Iamarino <125980333+SamIamarino@users.noreply.github.com>
2025-04-01 15:04:46 -0300

    feat: criação MobileApp Component

--- academia-evolucao/src/components/mobileAppMkt/MobileAppMkt.jsx
+++ academia-evolucao/src/components/mobileAppMkt/MobileAppMkt.jsx
@@ -1,6 +1,23 @@
+import './MobileAppMkt.css';
+
 export default function MobileAppMkt(){
 
     return(
-        <h1>teste</h1>
+        <div className='mobile-app-container'>
+            <div className='mobile-app-main-container'>
+                <div className='mobile-text-content'>
+                    <h1>Texto</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
+                    <ul>
+                        <li>Teste</li>
+                        <li>Teste</li>
+                        <li>Teste</li>
+                        <li>Teste</li>
+                        <li>Teste</li>
+                    </ul>
+                    <img src="" alt="Imagem Celular" />
+                </div>
+            </div>
+        </div>
     );
 }
