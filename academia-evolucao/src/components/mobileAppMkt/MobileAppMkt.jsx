09a5342e2fe501ffddc54848c78ee65780e77cba
Samuel Ferla Iamarino <125980333+SamIamarino@users.noreply.github.com>
2025-04-02 14:38:08 -0300

    feat: Desenvolvimento componente MobileAppMkt

--- academia-evolucao/src/components/mobileAppMkt/MobileAppMkt.jsx
+++ academia-evolucao/src/components/mobileAppMkt/MobileAppMkt.jsx
@@ -1,22 +1,50 @@
 import './MobileAppMkt.css';
-
+import celularFoto from '../../assets/celular-app.png'
+import { useState, useEffect } from 'react';
 export default function MobileAppMkt(){
 
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 750);
+
+    useEffect(() => {
+        const updateValues = () => {
+            setIsMobile(window.innerWidth <= 750);
+        };
+
+        window.addEventListener("resize", updateValues);
+        updateValues();
+
+        return () => {
+            window.removeEventListener("resize", updateValues);
+        };
+    }, []); 
+
+    console.log(isMobile);
+
     return(
+        // que tenha em mente que este eh o conteudo do desktop
+
         <div className='mobile-app-container'>
             <div className='mobile-app-main-container'>
                 <div className='mobile-text-content'>
-                    <h1>Texto</h1>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
-                    <ul>
-                        <li>Teste</li>
-                        <li>Teste</li>
-                        <li>Teste</li>
-                        <li>Teste</li>
-                        <li>Teste</li>
-                    </ul>
-                    <img src="" alt="Imagem Celular" />
+                    {/* talvez tenha q colocar td em div aqui */}
+                    <div>
+                        <h1 className='mobile-container-title'>Baixe nosso app tecnofit para auxilio nos treinos.</h1>
+                    </div>
+                    <div>
+                        <p className='mobile-container-paragraph'>Tecnofit alem de te auxiliar nos treinos é a principal rede de comunicação com os alunos</p>
+                    </div>
+                    <div>
+                        <ul className='mobile-item-list'>
+                            <li>Informações</li>
+                            <li>Horários</li>
+                            <li>Reserva de Aulas</li>
+                            <li>Atualizações</li>
+                            <li>Eventos</li>
+                        </ul>
+                    </div>
+                    
                 </div>
+                    <img src={celularFoto} alt="Imagem Celular" className='smartphone-img'/>
             </div>
         </div>
     );
