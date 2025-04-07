765ce0cf2d2c93d953205f56f8d1073949981f60
Samuel Ferla Iamarino <125980333+SamIamarino@users.noreply.github.com>
2025-04-07 13:32:22 -0300

    feat: MobileAppMkt responsividade

--- academia-evolucao/src/components/mobileAppMkt/MobileAppMkt.jsx
+++ academia-evolucao/src/components/mobileAppMkt/MobileAppMkt.jsx
@@ -1,28 +1,11 @@
 import './MobileAppMkt.css';
 import celularFoto from '../../assets/celular-app.png'
+import calularFotoSemSombra from '../../assets/celular_without-shadow.png'
 import { useState, useEffect } from 'react';
-export default function MobileAppMkt(){
 
-    const [isMobile, setIsMobile] = useState(window.innerWidth <= 750);
 
-    useEffect(() => {
-        const updateValues = () => {
-            setIsMobile(window.innerWidth <= 750);
-        };
 
-        window.addEventListener("resize", updateValues);
-        updateValues();
-
-        return () => {
-            window.removeEventListener("resize", updateValues);
-        };
-    }, []); 
-
-    console.log(isMobile);
-
-    return(
-        // que tenha em mente que este eh o conteudo do desktop
-
+const desktopContent = (
         <div className='mobile-app-container'>
             <div className='mobile-app-main-container'>
                 <div className='mobile-text-content'>
@@ -47,5 +30,43 @@ export default function MobileAppMkt(){
                     <img src={celularFoto} alt="Imagem Celular" className='smartphone-img'/>
             </div>
         </div>
+        );
+
+const mobileContent = (
+    <div className='mobile-content-container'>
+        <img src={calularFotoSemSombra} alt="Imagem Celular" className='smartphone-img'/>
+        <ul className='mobile-item-list'>
+            <li>Informações</li>
+            <li>Horários</li>
+            <li>Reserva de Aulas</li>
+            <li>Atualizações</li>
+            <li>Eventos</li>
+        </ul>
+    </div>
+);
+
+
+export default function MobileAppMkt(){
+
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 750);
+
+    useEffect(() => {
+        const updateValues = () => {
+            setIsMobile(window.innerWidth <= 750);
+        };
+
+        window.addEventListener("resize", updateValues);
+        updateValues();
+
+        return () => {
+            window.removeEventListener("resize", updateValues);
+        };
+    }, []); 
+
+
+    return(
+        <>
+            {isMobile ? mobileContent : desktopContent}
+        </>
     );
 }
