d36a007e417de74114e6286d8e89c0c74b1a05df
Samuel Ferla Iamarino <125980333+SamIamarino@users.noreply.github.com>
2025-04-14 12:31:54 -0300

    feat: desenvolvimento Treine aqui container

---
+++ academia-evolucao/src/components/TreineAquiContainer/TreineAquiContainer.jsx
@@ -0,0 +1,13 @@
+import './TreineAquiContainer.css';
+import WhatsAppButton from '../whatsappButton/WhatsappButton';
+export default function TreineAquiContainer({darkTheme}){
+    return(
+        <div className='treine-aqui-container'>
+            <div className='treine-aqui-content'>
+                <h1 className='treine-aqui-title'>Agende seu treino conosco!!</h1>
+                {/* DarkTheme True aqui sempre pois no tema claro ele n pode mudar de classe */}
+                <WhatsAppButton darkTheme={true}>Fale com a gente</WhatsAppButton>
+            </div>
+        </div>
+    );
+}
