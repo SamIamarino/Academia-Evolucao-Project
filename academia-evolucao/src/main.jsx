fd40367f9fe1404239a25a60d9a3036a3e6dc158
Samuel Ferla Iamarino <125980333+SamIamarino@users.noreply.github.com>
2025-05-21 09:37:13 -0300

    fix: arrumando roteamento novamente

--- academia-evolucao/src/main.jsx
+++ academia-evolucao/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter , Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import AboutPage from './pages/AboutPage.jsx'
@@ -10,10 +10,10 @@ import AboutPage from './pages/AboutPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter basename="/Academia-Evolucao-Project">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/aboutUs" element={<AboutPage />} />
+        <Route path="aboutUs" element={<AboutPage />} />
       </Routes>
     </HashRouter>
   </StrictMode>,
